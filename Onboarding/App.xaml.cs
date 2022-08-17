e835c0babc7d91d8fbd883b23d8e96a802d8602a
M. Naveed Qamar <naveedqamar6500@gmail.com>
2022-08-17 14:22:05 +0500

    Code Commit

---
+++ Onboarding/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Onboarding
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            VersionTracking.Track();
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Onboarding/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using Onboarding.Feature;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Onboarding
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            if (VersionTracking.IsFirstLaunchEver)
+            {
+                Navigation.PushModalAsync(new OnboardingPage());
+            }
+        }
+    }
+}
